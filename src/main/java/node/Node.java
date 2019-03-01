c1ab377b180a98e1352716be1bed04a36c0b6398
MatteoPreti <matteo.preti@mail.polimi.it>
2019-03-01 18:34:58 +0100

    Lookup methods

--- src/main/java/node/Node.java
+++ src/main/java/node/Node.java
@@ -2,20 +2,30 @@ package node;
 
 import network.SocketNodeListener;
 
-import java.util.List;
+import java.util.HashMap;
+
+import java.util.Map;
 import java.util.concurrent.Executors;
 
 public class Node {
     private String ipAddress;
+    private int nodeId;
     private Node successor;
     private Node predecessor;
-    private List<Node> fingerTable; //questa può diventare una map tra il socket e il nodo collegato
+    private Map<Integer, Node> fingerTable;
+
+    public static final int DIM_FINGER_TABLE = 4;
 
     public Node(String ipAddress){
         this.ipAddress = ipAddress;
+        this.nodeId=0;
         this.successor = null;
         this.predecessor = null;
-        this.fingerTable = null;
+        this.fingerTable = new HashMap<>();
+    }
+
+    public int getNodeId() {
+        return nodeId;
     }
 
     public void create(){
@@ -25,10 +35,33 @@ public class Node {
     }
 
     public void join (Node n){
-
+        predecessor = null;
+        successor=n.find_successor(this.nodeId);
     }
 
     public void stabilize(){
+
+    }
+
+    public Node find_successor(int id){
+        Node next;
+        if(id > this.getNodeId() && id <= this.successor.getNodeId())
+            return this.successor;
+        else {
+            next = closest_preceding_node(id);
+            return next.find_successor(id);
+        }
+    }
+
+    public Node closest_preceding_node(int id){
+        int nodeIndex;
+        for (int i=DIM_FINGER_TABLE; i > 1; i--){
+            nodeIndex=  fingerTable.get(i).getNodeId();
+            if ( nodeIndex < id && nodeIndex > this.getNodeId())
+                return  fingerTable.get(i);
+        }
+
+        return this;
 
     }
 
