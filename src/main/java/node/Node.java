ded8c52d63a0d4ed7ae486ca734b6ca9f6fed137
davidesiciliano <37239185+davidesiciliano@users.noreply.github.com>
2019-03-04 19:24:05 +0100

    aggiustamenti
    predecessore
    successore
    notify

--- src/main/java/node/Node.java
+++ src/main/java/node/Node.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Node implements NodeInterface {
@@ -18,7 +19,7 @@ public class Node implements NodeInterface {
     private NodeInterface successor; //TODO questi dovrebbero essere NodeCommunicator
     private NodeInterface predecessor;
     private Map<Integer, NodeInterface> fingerTable;
-    private static final int DIM_FINGER_TABLE = 4; //questo poi potremmo metterlo variabile scelto nella create
+    private static final int DIM_FINGER_TABLE = 3; //questo poi potremmo metterlo variabile scelto nella create
     private int next;
 
     public Node(String ipAddress) {
@@ -27,7 +28,7 @@ public class Node implements NodeInterface {
         this.successor = null;
         this.predecessor = null;
         this.fingerTable = new HashMap<>();
-        this.next = 1;
+        this.next = 0;
     }
 
     public void create() {
@@ -48,11 +49,12 @@ public class Node implements NodeInterface {
 
     @Override
     public void stabilize() {
-        NodeInterface x = this.getSuccessor().getPredecessor();
+        //controllo su null predecess
+        NodeInterface x = successor.getPredecessor();
         long nodeIndex = x.getNodeId();
-        long oldSucID = this.successor.getNodeId();
-        if (nodeIndex < oldSucID && nodeIndex > this.getNodeId())
-            this.successor = x;
+        long oldSucID = successor.getNodeId();
+        if (checkInterval(getNodeId(),nodeIndex,oldSucID))
+            successor = x;
 
         this.successor.notify(this);
 
@@ -61,10 +63,11 @@ public class Node implements NodeInterface {
     @Override
     public NodeInterface findSuccessor(long id) throws IOException {
         NodeInterface next;
-        if (id > this.getNodeId() && id <= this.successor.getNodeId())
+        if (checkInterval(nodeId, id, successor.getNodeId()))
             return this.successor;
         else {
             next = closestPrecedingNode(id);
+            out.println(id);
             return next.findSuccessor(id);
         }
     }
@@ -72,22 +75,39 @@ public class Node implements NodeInterface {
     @Override
     public NodeInterface closestPrecedingNode(long id) {
         long nodeIndex;
-        for (int i = DIM_FINGER_TABLE; i > 1; i--) {
-            if (fingerTable.containsKey(i)) {
-                nodeIndex = fingerTable.get(i).getNodeId();
-                if (nodeIndex < id && nodeIndex > this.getNodeId())
-                    return fingerTable.get(i);
-            }
+        for (int i = DIM_FINGER_TABLE-1; i >= 0; i--) {
+            nodeIndex = fingerTable.get(i).getNodeId();
+            if (nodeIndex < id && nodeIndex > this.getNodeId())
+                return fingerTable.get(i);
         }
         return this;
     }
 
     @Override
     public void notify(Node n) {
-        long nIndex = n.getNodeId();
-        long predIndex = this.predecessor.getNodeId();
-        if (this.predecessor == null || (nIndex > predIndex && nIndex < this.getNodeId()))
-            this.predecessor = n;
+        if(predecessor == null)
+            predecessor = n;
+        else {
+            long index = n.getNodeId();
+            long predIndex = predecessor.getNodeId();
+            if (checkInterval(predIndex, index, getNodeId()))
+                predecessor = n;
+        }
+    }
+
+    public boolean checkInterval(long pred, long index, long succ){
+        if(pred == succ)
+            return true;
+        if(pred > succ){
+            if((index > pred && index < Math.pow(2,DIM_FINGER_TABLE) - 1) || (index > 0 && index < succ))
+                return true;
+            return false;
+        }
+        else{
+            if(index > pred && index < succ)
+                return true;
+            return false;
+        }
     }
 
     @Override
@@ -95,10 +115,11 @@ public class Node implements NodeInterface {
         long idToFind;
         next = next + 1;
         if (next > DIM_FINGER_TABLE)
-            next = 1;
+            next = 0;
         //fix cast
-        idToFind = this.getNodeId() + ((long) Math.pow(2, next - 1));
-        fingerTable.replace(next, findSuccessor(idToFind));
+        idToFind = (nodeId + ((long) Math.pow(2, next - 1))) % (long) Math.pow(2,DIM_FINGER_TABLE);
+
+        fingerTable.replace(next -1, findSuccessor(idToFind));
 
     }
 
@@ -113,7 +134,7 @@ public class Node implements NodeInterface {
     }
 
     public void createFingerTable(){
-        for(int i = 1; i <= DIM_FINGER_TABLE; i++){
+        for(int i = 0; i <= DIM_FINGER_TABLE-1; i++){
             fingerTable.put(i, this);
         }
     }
@@ -147,14 +168,80 @@ public class Node implements NodeInterface {
     public void join(Node node) throws IOException {
         predecessor = null;
         successor = node.findSuccessor(this.nodeId);
+        createFingerTable();
         out.println("Gol di Pavoletti");
     }
 
-    public static void main(String[] args) throws IOException {
-        Node node1 = new Node(1);
-        Node node2 = new Node(2);
+    public Map<Integer, NodeInterface> getFingerTable() {
+        return fingerTable;
+    }
 
-        node1.create();
+    public static void main(String[] args) throws IOException {
+        Node node0 = new Node(0);
+        Node node1 = new Node(1);
+        Node node5 = new Node(5);
+
+        node0.create();
+        node1.join(node0);
+        node5.join(node1);
+        out.println("0:");
+        //out.println(node0.getPredecessor().getNodeId());
+        out.println(node0.getSuccessor().getNodeId());
+        out.println("1:");
+        //out.println(node1.getPredecessor().getNodeId());
+        out.println(node1.getSuccessor().getNodeId());
+        out.println("5:");
+        //out.println(node5.getPredecessor().getNodeId());
+        out.println(node5.getSuccessor().getNodeId());
+
+        node0.notify(node1);
+        node1.notify(node0);
+        node0.notify(node5);
+        node5.notify(node1);
+        node0.stabilize();
+        node1.stabilize();
+        node5.stabilize();
+        node0.stabilize();
+        node0.stabilize();
+        out.println("PREDECESSORE\nSUCCESSORE");
+        out.println("0:----------------------");
+        out.println(node0.getPredecessor().getNodeId());
+        out.println(node0.getSuccessor().getNodeId());
+        out.println("1:-----------------------");
+        out.println(node1.getPredecessor().getNodeId());
+        out.println(node1.getSuccessor().getNodeId());
+        out.println("5:-----------------------");
+        out.println(node5.getPredecessor().getNodeId());
+        out.println(node5.getSuccessor().getNodeId());
+        out.println("\n\nFINGER TABLE");
+
+        for(int i = 0; i<1; i++) {
+            node0.fixFingers();
+            node0.fixFingers();
+            node0.fixFingers();
+            node1.fixFingers();
+            node1.fixFingers();
+            node1.fixFingers();
+            node5.fixFingers();
+            node5.fixFingers();
+            node5.fixFingers();
+        }
+
+        out.println("Pavoletti\n\n");
+        out.println("Nodo 0: ------------------------");
+        out.println(node0.getFingerTable().get(0).getNodeId());
+        out.println(node0.getFingerTable().get(1).getNodeId());
+        out.println(node0.getFingerTable().get(2).getNodeId());
+        out.println("Nodo 1: ------------------------");
+        out.println(node1.getFingerTable().get(0).getNodeId());
+        out.println(node1.getFingerTable().get(1).getNodeId());
+        out.println(node1.getFingerTable().get(2).getNodeId());
+        out.println("Nodo 5: ------------------------");
+        out.println(node5.getFingerTable().get(0).getNodeId());
+        out.println(node5.getFingerTable().get(1).getNodeId());
+        out.println(node5.getFingerTable().get(2).getNodeId());
+
+
 
 
     }
