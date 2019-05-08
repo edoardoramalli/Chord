85338fa60ba9295c7de3cf44c7440bfd299cc2ce
davidesiciliano <37239185+davidesiciliano@users.noreply.github.com>
2019-05-08 15:43:17 +0200

    JavaDoc SocketNodeListener

--- src/main/java/network/SocketNodeListener.java
+++ src/main/java/network/SocketNodeListener.java
@@ -9,16 +9,27 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Class that manage the accepting of incoming connections
+ */
 public class SocketNodeListener implements Runnable, Serializable {
     private int socketPort;
     private transient Node node;
     private static Boolean active = true;
 
+    /**
+     * @param node node in listening (that will be passed as argument for the creation of SocketNode)
+     * @param socketPort socketPort to which the node starts accepting incoming connections
+     */
     public SocketNodeListener(Node node, int socketPort){
         this.node = node;
         this.socketPort = socketPort;
     }
 
+    /**
+     * Creates a ServerSocket on the socketPort that remains in wait of connection,
+     * and foreach connection starts a SocketNode on a different thread
+     */
     @Override
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(socketPort)) {
